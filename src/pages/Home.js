0fc659bf3b8ce0725f6dd1b1dcb155d55056b920
m4nik1 <maniksoomro@hotmail.com>
2022-01-16 17:29:45 -0500

    It was breaking earlier now it is fixed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,12 @@
 import React from "react";
-
+import "../CSS/HomePage.css";
 
 const Home = () => {
-
-}
+  return (
+    <div>
+      <h1>Welcome to the homepage!</h1>
+    </div>
+  );
+};
 
 export default Home;
