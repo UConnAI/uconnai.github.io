5302e011c2a8436d69e2cce7e0e53df42701139f
m4nik1 <maniksoomro@hotmail.com>
2022-01-16 17:05:00 -0500

    Added Components, CSS, Pages for organization and created page files also wired the website to go straight to Homepage also (Fuck you Ronak)

---
+++ src/Pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import '../CSS/HomePage.css'
+
+
+
+const Home = () => {
+    return(
+        <div>
+            <h1>Welcome to the homepage!</h1>
+        </div>
+    )
+}
+
+export default Home;
