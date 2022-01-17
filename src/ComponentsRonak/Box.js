bb218417b8e87a1ae1eaf8459c74d7423b7b50fd
RonakGSahu <ronaksahu18@gmail.com>
2022-01-16 21:15:17 -0500

    Ronak DA Great work on this stupid CSS

---
+++ src/ComponentsRonak/Box.js
@@ -0,0 +1,14 @@
+import React from "react";
+import TestPicture from '../image0.jpg';
+import '../ComponentsRonak/RoundedPicture.css'
+import RoundedPicture from './RoundedPicture';
+function Box (props) {
+    return (
+    <div className="MemberBox">
+        <RoundedPicture image = {TestPicture} person = "React" />
+        <h1 className="MemberNameText">{props.Name}</h1> 
+        <h3 className="MemberPositionText">{props.Position}</h3>
+    </div>
+    )
+}
+export default Box;
--- src/pages/TeamPage.js
+++ src/pages/TeamPage.js
@@ -1,1 +1,12 @@
 import React from "react";
+
+import InformationBox from "../ComponentsRonak/InformationBox";
+import "../ComponentsRonak/InfoBox.css"
+
+function Trying(){
+    return (
+    <InformationBox Name = "Jisoo Kim" Position = "Better Person than Alex for anything" MajandMin = "Computer Science & Engineering, Applied Mathematics" Year = "2023" AdditionalInfo = "Hey There, IDK what I am supposed to write over here but i am trying to make this long just so that i can test this. Seems like I need to make this a little tad bit more longer than i had originally expected to test"/>
+    )
+
+}
+export default Trying;
