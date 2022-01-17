ea0fb23d6e06c1cc6b862c1b8977be85fc7046ef
Charitarth Chugh <37895518+charitarthchugh@users.noreply.github.com>
2022-01-17 17:02:37 -0500

    in progress nav bar

--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -1,1 +1,31 @@
 import React from "react";
+import "../CSS/NavagationBar.css";
+import "../CSS/logo.css";
+import { Link } from "react-router-dom";
+
+const NavagationBar = ({ sticky }) => {
+  return (
+    <nav className="navbar">
+      <header className="textHeader">
+        <Link to="/">UConn AI</Link>
+      </header>
+      <div className="navbar-link">
+        <Link to="/TeamPage" className="navbar-link-item">
+          Team
+        </Link>
+        <Link to="/ProjectsPage" className="navbar-link-item">
+          Projects
+        </Link>
+        <Link to="/TutorialPage" className="navbar-link-item">
+          Tutorials
+        </Link>
+        <button className="joinButton">
+          <Link to="JoinPage">
+            <div className="joinText">Join</div>
+          </Link>
+        </button>
+      </div>
+    </nav>
+  );
+};
+export default NavagationBar;
