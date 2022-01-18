e4b57259f5609556c798dc4a8d3ee8eaece96a30
RonakGSahu <ronaksahu18@gmail.com>
2022-01-17 23:49:15 -0500

    Finished the Footer Component and changed the ProjectTitle to have props in it

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import JoinPage from "./Pages/JoinPage";
 import NotFound from "./Pages/NotFound";
 import NavagationBar from "./Components/NavigationBar";
 import ProjectsPage from "./Pages/ProjectsPage";
-
+import Footer from "./Components/FooterComponent";
 function App() {
   return (
     <div className="App">
@@ -23,6 +23,7 @@ function App() {
         <Route path="/ProjectsPage" element={<ProjectsPage />} />
         <Route path="404" element={<NotFound />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
--- src/Pages/ProjectsPage.js
+++ src/Pages/ProjectsPage.js
@@ -1,7 +1,98 @@
 import React from "react";
 import LeftProjectTitle from "../Components/LeftProjectTitle";
+import RightProjectTitle from "../Components/RightProjectTitle";
+import Jisoo from "../image0.jpg";
 
 function ProjectsPage() {
-  return <LeftProjectTitle />;
+  return (
+    <div>
+      <LeftProjectTitle
+        Image={Jisoo}
+        Title="Kim Jisoo"
+        Description="(Copied from her fandom for testing ) Kim Ji-soo (Hangul: 김지수, born
+          January 3, 1995 in Gunpo, Gyeonggi Province, South Korea), better
+          known mononymously as Jisoo, is a South Korean singer and actress. She
+          is a member of BLACKPINK. Biography Early life Kim Ji-soo was born on
+          January 3, 1995, in Gunpo, Gyeonggi, South Korea. In 2011, Kim joined
+          YG Entertainment through auditions as a trainee. In 2015, Kim made a
+          cameo appearance in the KBS2 drama The Producers alongside label-mates
+          2NE1's Sandara Park and Winner's Kang Seung-yoon. In the same year Kim
+          featured in several advertisements including Samsonite alongside actor
+          Lee Min-ho, Smart Uniform, LG Electronics, and Nikon. Career Jisoo
+          became a YG Entertainment trainee in August 2011, when she was 16
+          years old, she trained five years for her singing career. In August
+          2016, she became a member of BLACKPINK, as a lead vocalist and
+          visualist in the group. Before she became a trainee at YG, she was
+          known by many for her beauty. She was very popular in school because
+          of her beautiful face and talents. She used to study at the School Of
+          Performing Arts High School. She eventually transferred schools when
+          her family moved to Seoul, South Korea. In 2012, she was revealed
+          through the “Who’s That Girl?” teaser by YG Entertainment, and it was
+          followed with two photos in January of the next year. On August 18,
+          2020, YG Entertainment announced that Jisoo will be making her acting
+          debut. She will be debuting as the lead role in the upcoming JTBC
+          drama, Snowdrop. The drama is expected to air sometime in 2021. On
+          December 30, 2020, it was announced that Jisoo will be having her solo
+          debut when the filming for her drama, Snowdrop is finished. On August
+          18, a video was posted onto JTBC Drama's YouTube channel announcing
+          Snowdrop as part of the upcoming Drama Line-up.[1] Impact and
+          influence In Gallup Korea's annual music poll for 2018, Jisoo was
+          ranked the tenth most popular idol in South Korea, receiving 4.8% of
+          the votes. In April 2019, she was ranked the 10th most followed K-pop
+          idol on Instagram, with 12.8 million followers. Endorsements KISS ME
+          (with Rosé) (2018) Dior Beauty (2019) itMICHAA (2021) Dior (2021)
+          CELEBe (2021) Filmography Music Videos Epik High – 스포일러(Spoiler) +
+          헤픈엔딩(Happen Ending) (2014) Hi Suhyun – 나는 달라(I’m Different)
+          (2014) Dramas 2015 - The Producers by KBS2 2017 - Part-Time Idol by
+          SBS 2019 - Arthdal Chronicles by TVN 2021 - Snowdrop by JTBC[2]
+          Commercials Samsonite Red with Lee Min Ho (2015) Nikon 1 J5 (2015)
+          Smart Uniform with iKon (2015-2016) LG with iKon (2016) Angel Stone
+          Game with Naver (2017)"
+      />
+      <RightProjectTitle
+        Image={Jisoo}
+        Title="Kim Jisoo"
+        Description="(Copied from her fandom for testing ) Kim Ji-soo (Hangul: 김지수, born
+          January 3, 1995 in Gunpo, Gyeonggi Province, South Korea), better
+          known mononymously as Jisoo, is a South Korean singer and actress. She
+          is a member of BLACKPINK. Biography Early life Kim Ji-soo was born on
+          January 3, 1995, in Gunpo, Gyeonggi, South Korea. In 2011, Kim joined
+          YG Entertainment through auditions as a trainee. In 2015, Kim made a
+          cameo appearance in the KBS2 drama The Producers alongside label-mates
+          2NE1's Sandara Park and Winner's Kang Seung-yoon. In the same year Kim
+          featured in several advertisements including Samsonite alongside actor
+          Lee Min-ho, Smart Uniform, LG Electronics, and Nikon. Career Jisoo
+          became a YG Entertainment trainee in August 2011, when she was 16
+          years old, she trained five years for her singing career. In August
+          2016, she became a member of BLACKPINK, as a lead vocalist and
+          visualist in the group. Before she became a trainee at YG, she was
+          known by many for her beauty. She was very popular in school because
+          of her beautiful face and talents. She used to study at the School Of
+          Performing Arts High School. She eventually transferred schools when
+          her family moved to Seoul, South Korea. In 2012, she was revealed
+          through the “Who’s That Girl?” teaser by YG Entertainment, and it was
+          followed with two photos in January of the next year. On August 18,
+          2020, YG Entertainment announced that Jisoo will be making her acting
+          debut. She will be debuting as the lead role in the upcoming JTBC
+          drama, Snowdrop. The drama is expected to air sometime in 2021. On
+          December 30, 2020, it was announced that Jisoo will be having her solo
+          debut when the filming for her drama, Snowdrop is finished. On August
+          18, a video was posted onto JTBC Drama's YouTube channel announcing
+          Snowdrop as part of the upcoming Drama Line-up.[1] Impact and
+          influence In Gallup Korea's annual music poll for 2018, Jisoo was
+          ranked the tenth most popular idol in South Korea, receiving 4.8% of
+          the votes. In April 2019, she was ranked the 10th most followed K-pop
+          idol on Instagram, with 12.8 million followers. Endorsements KISS ME
+          (with Rosé) (2018) Dior Beauty (2019) itMICHAA (2021) Dior (2021)
+          CELEBe (2021) Filmography Music Videos Epik High – 스포일러(Spoiler) +
+          헤픈엔딩(Happen Ending) (2014) Hi Suhyun – 나는 달라(I’m Different)
+          (2014) Dramas 2015 - The Producers by KBS2 2017 - Part-Time Idol by
+          SBS 2019 - Arthdal Chronicles by TVN 2021 - Snowdrop by JTBC[2]
+          Commercials Samsonite Red with Lee Min Ho (2015) Nikon 1 J5 (2015)
+          Smart Uniform with iKon (2015-2016) LG with iKon (2016) Angel Stone
+          Game with Naver (2017)"
+      />
+    </div>
+  );
 }
 export default ProjectsPage;
