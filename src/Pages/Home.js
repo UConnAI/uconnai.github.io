4fb8d3b887477b2b072b32dd3ca0520c73c83304
charitarthchugh <37895518+charitarthchugh@users.noreply.github.com>
2022-01-25 22:42:41 -0500

    Home page with calendar

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,12 +3,13 @@ import "../CSS/HomePage.css";
 import Box from "../Components/Box";
 import TeamPictures from "../Components/TeamPictures";
 import AlexMoment from "../assets/Alex-Moment.jpg";
-
+import Calendar from "../Components/Calendar";
 const HomePage = (element) => {
   return (
     <div style={{ marginTop: "7rem" }}>
       <h1>Welcome to the homepage!</h1>
       <p className="upcomingText">Upcoming Events</p>
+      <Calendar />
       <p className="teamText">The Team</p>
       <div className="teamPictures">
         <img className="teamPic" src={AlexMoment} />
